14e2d6ca7b0ae561703ac30754c2d7daaf7f670b
LucaSantosSP <lucas-santos20.17@hotmail.com>
2022-07-07 21:19:29 -0300

    Finalizada pagina de cadastro

---
+++ cadastro.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LDStore | Cadastro</title>
+    <link rel="stylesheet" href="CSS/registro.css">
+
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1>Cadastro</h1>
+        <p>Realize seu cadastro é rápido e fácil e tenha a possibilidade de adquirir nossos produtos</p>
+        <form>
+            <p>Preencha os campos abaixo com as informações necessárias *</p>
+
+            <label for="iNome">Nome&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</label>
+            <input type="text" name="nNome" id="iNome" required placeholder="Ex: Fulano"><br>
+
+            <label for="iSobrenome">Sobrenome&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</label>
+            <input type="text" name="nSobrenome" id="iSobrenome" required placeholder="Ex: Da Silva"><br>
+
+            <label for="iEmail">E-mail&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</label>
+            <input type="text" name="eEmail" id="iEmail" required placeholder="Ex: fulano@gmail.com"><br>
+            
+            <label for="iSenha">Senha&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</label>
+            <input type="text" name="eSenha" id="iSenha" required placeholder="*************"><br>
+
+            <label for="iRepSenha">Repetir Senha&nbsp&nbsp</label>
+            <input type="text" name="eRepSenha" id="iRepSenha" required placeholder="*************"><br>
+
+            <input type="submit" value="Cadastre-se" id="botCadastro">
+
+        </form>
+    </main>
+    <footer>
+        <h2>Formas de Pagamento</h2>
+        <img class="pagamentos" src="Imagens/visa-logo.png" alt="VISA">
+        <img class="pagamentos" src="Imagens/mastercard-logo.png" alt="Mastercard">
+        <img class="pagamentos" src="Imagens/hipercard-logo.png" alt="hypercard">
+        <img class="pagamentos" src="Imagens/elo-logo.png" alt="ELO">
+        <img class="pagamentos" src="Imagens/boleto.png" alt="Boleto">
+        <img class="pagamentos" src="Imagens/pix.png" alt="PIX">
+
+        <h3>Siga-nos nas redes sociais</h3>
+        <img id="sociais" src="Imagens/05.png" alt="Logodas redes sociais">
+
+        <p>Copyright © 2022-2022 LDS.com.br LTDA</p>
+    </footer>
+</body>
+</html>
